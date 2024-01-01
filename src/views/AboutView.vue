c66492b55bca68bd61f7f1dc28f093f3fc13806b
Alex Swan <alex@bold-it.com>
2023-12-31 22:06:42 -0600

    About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Created by Alex Swan</h1>
+    <p>
+      View the source on <a href="https://github.com/BoldBigflank/bank/" target="_blank">GitHub</a>
+    </p>
   </div>
 </template>
 
