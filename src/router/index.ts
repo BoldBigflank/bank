7ee4a8061acd52417695361751c83ead6ff62c88
Alex Swan <smashcubed@gmail.com>
2025-03-13 14:38:45 -0500

    hash history

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,13 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+    createRouter,
+    createWebHashHistory,
+    createWebHistory,
+} from "vue-router";
 import GameView from "../views/GameView.vue";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    // history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(),
     routes: [
         {
             path: "/",
